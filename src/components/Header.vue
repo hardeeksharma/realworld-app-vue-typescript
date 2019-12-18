fbfbca7a0403cd79d85fbd3578e39a3f4ed04520
hardeeksharma <hardeek.sharma@gmail.com>
2019-12-18 13:18:25 +0530

    Profile page route link added in header
    Signed-off-by: hardeeksharma <hardeek.sharma@gmail.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,17 +5,17 @@
             <ul class="nav navbar-nav pull-xs-right">
                 <li class="nav-item">
                     <!-- Add "active" class when you're on that page" -->
-                    <a class="nav-link active" href="">Home</a>
+                    <router-link class="nav-link active" to="/">Home</router-link>
                 </li>
                 <li v-if="username" class="nav-item">
-                    <a class="nav-link" href="">
+                    <router-link class="nav-link" to="/editor">
                         <i class="ion-compose"></i>&nbsp;New Post
-                    </a>
+                    </router-link>
                 </li>
                 <li v-if="username" class="nav-item">
-                    <a class="nav-link" href="">
+                    <router-link class="nav-link" to="/settings">
                         <i class="ion-gear-a"></i>&nbsp;Settings
-                    </a>
+                    </router-link>
                 </li>
                 <li v-if="username" class="nav-item">
                     <router-link class="nav-link" :to="'@' + username">
